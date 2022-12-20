0af0b04e66d95e4250f311bf2d2adf1b288bc5fd
Rodrigue <studentrodrigue.dumez@gmail.com>
2022-12-20 17:44:11 +0100

    Update, CameraNowRotateWhileAim

--- Assets/Scripts/Player/ThirdPersonController.cs
+++ Assets/Scripts/Player/ThirdPersonController.cs
@@ -108,6 +108,7 @@ namespace StarterAssets
         private StarterAssetsInputs _input;
         private GameObject _mainCamera;
         private GameObject _followCamera;
+        private bool _rotateOnMove = true;
 
         private const float _threshold = 0.01f;
 
@@ -268,7 +269,10 @@ namespace StarterAssets
                     RotationSmoothTime);
 
                 // rotate to face input direction relative to camera position
-                transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+                if(_rotateOnMove)
+                {
+                    transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+                }    
             }
 
 
@@ -424,6 +428,11 @@ namespace StarterAssets
         public void SetSensitivity(float newSensitivity)
         {
             Sensitivity = newSensitivity;
+        }
+
+        public void SetRotateOnMove(bool newRotateOnMove)
+        {
+            _rotateOnMove = newRotateOnMove;
         }
     }
 }
