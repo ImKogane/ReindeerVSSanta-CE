107d5517eb354caaccd26a163f332c8136831148
Americanos33 <65153418+Americanos33@users.noreply.github.com>
2022-12-20 17:23:59 +0100

    Client and host connecting to relay

--- Assets/Scripts/Other/NetworkManagerUI.cs
+++ Assets/Scripts/Other/NetworkManagerUI.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 using UnityEngine.UI;
 using Unity.Services.Core;
 using Unity.Services.Authentication;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using TMPro;
+
 
 public class NetworkManagerUI : MonoBehaviour
 {
@@ -11,6 +15,18 @@ public class NetworkManagerUI : MonoBehaviour
     [SerializeField] private Button createButton;
     [SerializeField] private Button joinButton;
     [SerializeField] private GameObject joinInput;
+
+    private void Awake(){
+
+        createButton.onClick.AddListener(() => {
+            CreateRelay();
+        });
+
+        joinButton.onClick.AddListener(() => {
+            JoinRelay(joinInput.GetComponent<TMP_InputField>().text);
+        });
+
+    }
 
     private async void Start(){
         await UnityServices.InitializeAsync();
@@ -20,6 +36,27 @@ public class NetworkManagerUI : MonoBehaviour
         };
 
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+
+    private async void CreateRelay(){
+        try {
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(3);
+
+            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+
+            joinInput.GetComponent<TMP_InputField>().text = joinCode;
+        } catch (RelayServiceException e){
+            Debug.Log("Relay service error: " + e.Message);
+        }
+    }
+
+    private async void JoinRelay(string joinCode){
+        try {
+            Debug.Log("JoinRelay: " + joinCode);
+            RelayService.Instance.JoinAllocationAsync(joinCode);
+        } catch (RelayServiceException e){
+            Debug.Log("Relay service error: " + e.Message);
+        }
     }
 
 }
